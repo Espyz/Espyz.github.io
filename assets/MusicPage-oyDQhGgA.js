16eaf440c93da022f7b17764c36cf8fb5bbc2ed0
Espyz <truharandrey1015@gmail.com>
2024-12-05 19:24:08 +0500

    Новый роутинг

--- assets/MusicPage-oyDQhGgA.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,t as c,A as r}from"./index-IrEhXHYh.js";const t={};function n(o,s){return c(),r("div",null,"Suck More")}const a=e(t,[["render",n],["__file","MusicPage.vue"]]);export{a as default};
